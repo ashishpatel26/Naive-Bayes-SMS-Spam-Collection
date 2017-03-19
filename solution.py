76f02701a1053837cc371c255fbcef40d7ebb464
Wojciech Tyziniec <wtyzi001@gold.ac.uk>
2017-03-19 03:15:26 +0000

    Import the dataset into a pandas dataframe

---
+++ solution.py
@@ -0,0 +1,16 @@
+import pandas as pd
+'''
+	Import the dataset into a pandas dataframe using the read_table method.
+	Because this is a tab separated dataset, we will be using '\t' as the value for the 'sep'
+	argument which specifies this format.
+
+	Also, rename the column names by specyfiing a list ['label', 'sms_message'] to the 'names' argument of read_table()
+	Print the first five values of the dataframe with the new column names.
+'''
+df = pd.read_table('SMSSpamCollection',
+                   sep='\t', 
+                   header=None, 
+                   names=['label', 'sms_message'])
+
+
+print(df.head())
